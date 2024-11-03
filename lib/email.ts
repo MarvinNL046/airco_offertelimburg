b91ce0d28654bb8ff5ea4fc903ac000a1882ee6e
Marvin046 <info@staycoolairco.nl>
2024-11-03 14:57:48 +0100

    Overgestapt naar Mailgun voor e-mailverzending

--- lib/email.ts
+++ lib/email.ts
@@ -4,7 +4,7 @@ import Mailgun from 'mailgun.js';
 const mailgun = new Mailgun(formData);
 const mg = mailgun.client({
   username: 'api',
-  key: process.env.MAILGUN_API_KEY as string, // Zorg dat deze variabele is ingesteld in je .env bestand
+  key: process.env.MAILGUN_API_KEY as string,
 });
 
 interface EmailData {
